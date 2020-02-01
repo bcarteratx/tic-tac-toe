d5ff6605d920e961bf654ec618f211cb46643847
Brandon Carter <bcarteratx@gmail.com>
2020-01-31 21:12:15 -0600

    create basic html structure and 3x3 grid

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*------Constants------*/
+
+
+
+/*------ App's State (Variables)------*/
+const board;
+const turn;
+const winner;
+
+/*------Cached Element References------*/
+
+// You might choose to put your game status here
+
+/*------Event Listeners------*/
+
+// This is where you should put the event listener
+// for a mouse-click
+
+/*------Functions------*/
+init();
+// Where you set your initial state, setting up 
+// what the board will look like upon loading
+
+// On-Click function:
+// Set up what happens when one of the elements
+// is clicked
+
+
+// Check winner function:
+// Checks the current state of the board for
+// a winner and changes the state of the winner
+// variable if so
+
+
+// Render function:
+// Displays the current state of the board
+// on the page, updating the elements to reflect
+// either X or O depending on whose turn it is
